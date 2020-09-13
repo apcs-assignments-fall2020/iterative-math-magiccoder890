0e5a203ed960bace05a84795855fea0f31984d00
magiccoder890 <23fzhu@athenian.org>
2020-09-13 08:23:38 +0000

    This is Bert's homework for iterative.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,36 +1,53 @@
 import java.util.Scanner;
-
 public class MyMain {
-    
-    // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double m = 0;
+        double n = 1;
+        while (m - n > 0.00001 || m - n < -0.00001) {
+            m = (n + (x / n)) / 2 ;
+            n = m;
+            m = (n + (x / n)) / 2 ;
+        }
+        System.out.print(n);
+        return n;
     }
-
-    // Calculates the factorial of a number
-    public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+    public static double factorial(int test_int) {
+        double answer = 1;
+        if (test_int == 0) {
+            System.out.print("The factorial of 0 is 1");
+        }
+        else {
+            while (test_int > 1) {
+                answer = (int) (answer * test_int);
+                test_int--;
+            }
+        }
+        //System.out.print("\n" +answer);
+        return answer;
     }
-
-    // Calculates the value for the math constant e iteratively
-    // goes until it calculates an answer less than 0.00000000001 
-    // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int start_num = 1;
+        double e_value = 1;
+        while (true){
+            e_value += 1/factorial(start_num);
+            if (Math.E - e_value < 0.0000001) {
+                break;
+            }
+            else {
+                start_num ++;
+            }
+        }
+        System.out.print( e_value);
+        return e_value;
     }
-    
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
-
-        scan.close();
+    public static void main (String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input what number that you want to test");
+        double test = scanner.nextDouble();
+        babylonian(test);
+        System.out.print("Please take an integar as input");
+        int test_int = scanner.nextInt();
+        factorial(test_int);
+        calculateE();
     }
 }
